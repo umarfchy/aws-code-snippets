1f8d3978ed95c04c78278899e6107284e77daee2
umarfchy <umarfchy@gmail.com>
2021-11-20 01:06:04 +0600

    added code to list objects

--- s3.js
+++ s3.js
@@ -1,19 +1,21 @@
+//dependencies 
 const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
 
+// configs
+const config = {
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+};
+
+AWS.config.update(config);
+s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+const targetBucket = process.env.STORAGE_BUCKET;
+
 // put an object to s3
 const getS3Url = async (name) => {
-  const config = {
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.COVER_IMAGE_ACCESS_KEY,
-    secretAccessKey: process.env.COVER_IMAGE_SECRET_KEY,
-  };
-
-  AWS.config.update(config);
-  s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-
-  targetBucket = process.env.STORAGE_BUCKET;
-
   try {
     const salt = uuidv4(); // generate random id. This ensures uniqness.
     const fileNameWithFullPath = `folder1/folder2/${name}-${salt}`;
@@ -45,7 +47,6 @@ const removeS3Object = async (existingUrl) => {
   };
   AWS.config.update(config);
   s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-  targetBucket = process.env.STORAGE_BUCKET;
 
   const objectPath = existingUrl.split("amazonaws.com/")[1];
   const removeParams = { Bucket: targetBucket, Key: objectPath };
@@ -54,5 +55,18 @@ const removeS3Object = async (existingUrl) => {
   } catch (error) {
     console.log("Error on deleting data from S3 bucket.");
     throw { message: "Something went wrong while deleting the data." };
+  }
+};
+
+// list objects in s3 bucket
+const listS3Contents = async () => {
+  const bucketParams = {
+    Bucket: targetBucket,
+  };
+  try {
+    const data = await s3.listObjects(bucketParams).promise();
+    console.log("S3 contents:", data);
+  } catch (err) {
+    console.log("Error:", err);
   }
 };
